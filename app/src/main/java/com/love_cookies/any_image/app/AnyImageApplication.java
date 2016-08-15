a88cf4e9bbd135d4d39cd5d4dc2874c0dcd19ccc
Cookie <tsubasa-kun@foxmail.com>
2016-08-15 10:54:52 +0800

    first commit

---
+++ app/src/main/java/com/love_cookies/any_image/app/AnyImageApplication.java
@@ -0,0 +1,11 @@
+package com.love_cookies.any_image.app;
+
+import com.love_cookies.cookie_library.application.BaseApplication;
+
+/**
+ * Created by xiekun on 2016/8/15 0015.
+ *
+ * 应用的Application
+ */
+public class AnyImageApplication extends BaseApplication{
+}
