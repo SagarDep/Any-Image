b4cd67c6c8084b8e01c430d5309eb9e40c8154f2
Cookie <tsubasa-kun@foxmail.com>
2016-09-12 15:26:54 +0800

    图片详情页添加loading状态显示

--- app/src/main/java/com/love_cookies/any_image/view/activity/DetailActivity.java
+++ app/src/main/java/com/love_cookies/any_image/view/activity/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.love_cookies.any_image.view.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -7,9 +8,13 @@ import android.view.WindowManager;
 import com.love_cookies.any_image.R;
 import com.love_cookies.any_image.app.AnyImageApplication;
 import com.love_cookies.any_image.config.AppConfig;
+import com.love_cookies.any_image.view.widget.AutoSwipeRefreshLayout;
 import com.love_cookies.cookie_library.activity.BaseActivity;
+import com.love_cookies.cookie_library.utils.ScreenUtils;
+import com.love_cookies.cookie_library.utils.ToastUtils;
 import com.love_cookies.cookie_library.widget.PinchImageView;
 
+import org.xutils.common.Callback;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
@@ -22,6 +27,8 @@ import org.xutils.x;
 @ContentView(R.layout.activity_detail)
 public class DetailActivity extends BaseActivity {
 
+    @ViewInject(R.id.loading_layout)
+    private AutoSwipeRefreshLayout loadingLayout;
     @ViewInject(R.id.image_iv)
     private PinchImageView imageView;
 
@@ -32,7 +39,31 @@ public class DetailActivity extends BaseActivity {
     @Override
     public void initWidget(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        x.image().bind(imageView, AppConfig.IMAGE_1920x1080 + getIntent().getExtras().getString("id"), AnyImageApplication.NormalImageOptions);
+        //定位到屏幕中央
+        loadingLayout.setProgressViewOffset(false, ScreenUtils.getScreenHeight(this) / 2, ScreenUtils.getScreenHeight(this) / 2);
+        loadingLayout.autoRefresh();
+        x.image().loadDrawable(AppConfig.IMAGE_1920x1080 + getIntent().getExtras().getString("id"), AnyImageApplication.NormalImageOptions, new Callback.CommonCallback<Drawable>() {
+            @Override
+            public void onSuccess(Drawable result) {
+                imageView.setImageDrawable(result);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                ToastUtils.show(DetailActivity.this, ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+            }
+
+            @Override
+            public void onFinished() {
+                loadingLayout.setRefreshing(false);
+                //设为不可用，以防下拉会调用刷新
+                loadingLayout.setEnabled(false);
+            }
+        });
     }
 
     /**
